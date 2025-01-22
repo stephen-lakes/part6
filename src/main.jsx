f565943d94e1547aeb035756b64b4addf38f2c02
stephen-lakes <oluyomiolamilekan99@gmail.com>
2025-01-22 12:23:17 +0100

    separate creating actions into separate functions

--- src/main.jsx
+++ src/main.jsx
@@ -43,28 +43,34 @@ const generateId = () => {
   Number((Math.random() * 1000000).toFixed(0));
 };
 
+const createNote = (content) => {
+  return {
+    type: "NEW_NOTE",
+    payload: {
+      content,
+      important: false,
+      id: generateId(),
+    },
+  };
+};
+
+const toggleImportanceOf = (id) => {
+  return {
+    type: "TOGGLE_IMPORTANCE",
+    payload: { id },
+  };
+};
+
 const App = () => {
   const addNote = (event) => {
     event.preventDefault();
     const content = event.target.note.value;
     event.target.note.value = "";
-    store.dispatch({
-      type: "NEW_NOTE",
-      payload: {
-        content,
-        important: false,
-        id: generateId(),
-      },
-    });
+    store.dispatch(createNote(content));
   };
 
   const toggleImportance = (id) => {
-    store.dispatch({
-      type: "TOGGLE_IMPORTANCE",
-      payload: {
-        id,
-      },
-    });
+    store.dispatch(toggleImportanceOf(id));
   };
 
   return (
@@ -75,9 +81,7 @@ const App = () => {
       </form>
       <ul>
         {store.getState().map((note) => (
-          <li key={note.id}
-          onClick={()=> toggleImportance(note.id)}
-          >
+          <li key={note.id} onClick={() => toggleImportance(note.id)}>
             {note.content} <strong>{note.important ? "important" : ""}</strong>
           </li>
         ))}
