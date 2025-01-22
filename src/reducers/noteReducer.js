0af912f531002156c03d3972785f2e718c50f38e
stephen-lakes <oluyomiolamilekan99@gmail.com>
2025-01-22 12:46:13 +0100

    feat: implement note reducer and action creators
    - Implement noteReducer to handle NEW_NOTE action
    - Add generateId function to create unique IDs for notes
    - Create createNote action creator to dispatch NEW_NOTE action
    - Create toggleImportanceOf action creator to dispatch TOGGLE_IMPORTANCE
      action

---
+++ src/reducers/noteReducer.js
@@ -0,0 +1,31 @@
+const noteReducer = (state = [], action) => {
+  if (action.type === "NEW_NOTE") {
+    return state.concat(action.payload);
+  }
+
+  return state;
+};
+
+export const generateId = () => {
+  Number((Math.random() * 1000000).toFixed(0));
+};
+
+export const createNote = (content) => {
+  return {
+    type: "NEW_NOTE",
+    payload: {
+      content,
+      important: false,
+      id: generateId(),
+    },
+  };
+};
+
+export const toggleImportanceOf = (id) => {
+  return {
+    type: "TOGGLE_IMPORTANCE",
+    payload: { id },
+  };
+};
+
+export default noteReducer;
