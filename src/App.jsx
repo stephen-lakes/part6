482d063f23b6bde72541051767c14062d0ea6051
stephen-lakes <oluyomiolamilekan99@gmail.com>
2025-01-23 02:11:41 +0100

    feat: Add filter functionality with radio buttons in App component
    - Implemented filterSelected function to handle filter changes
    - Added radio buttons for filtering notes by all, important, and
      nonimportant
      - Enhanced filter UI for better user experience
      - Logged selected filter value to console for debugging

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,36 @@ import NewNote from "./components/NewNote";
 import Notes from "./components/Notes";
 
 const App = () => {
+  const filterSelected = (value) => {
+    console.log(value);
+  };
+
   return (
     <div>
       <NewNote />
+      <div>
+        all
+        <input
+          type="radio"
+          name="filter"
+          id=""
+          onChange={() => filterSelected("ALL")}
+        />
+        important
+        <input
+          type="radio"
+          name="filter"
+          id=""
+          onChange={() => filterSelected("NIMPORTANT")}
+        />
+        nonimportant
+        <input
+          type="radio"
+          name="filter"
+          id=""
+          onChange={() => filterSelected("NONIMPORTANT")}
+        />
+      </div>
       <Notes />
     </div>
   );
