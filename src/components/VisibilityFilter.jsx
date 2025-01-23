2fb4b45b38fe1c867415f1558e59f3af5ff664ef
stephen-lakes <oluyomiolamilekan99@gmail.com>
2025-01-23 03:07:44 +0100

    fix: use useDispatch() instead of useDispatch

--- src/components/VisibilityFilter.jsx
+++ src/components/VisibilityFilter.jsx
@@ -2,7 +2,7 @@ import { useDispatch } from "react-redux";
 import { filterChange } from "../reducers/filterReducer";
 
 function VisibilityFilter() {
-  const dispatch = useDispatch;
+  const dispatch = useDispatch();
 
   return (
     <div>
