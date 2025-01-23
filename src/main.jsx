6a49520e93de24326a51de28a5b391e893e979dc
stephen-lakes <oluyomiolamilekan99@gmail.com>
2025-01-23 02:45:27 +0100

    refactor: Correct filterChange action creator payload structure
    - Updated filterChange action creator to use payload property
    - Ensured consistent action structure for SET_FILTER action
    - Improved readability and maintainability of action creators

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,25 @@
 import ReactDOM from "react-dom/client";
 
-import { createStore } from "redux";
+import { createStore, combineReducers } from "redux";
 import App from "./App";
 import { Provider } from "react-redux";
-import noteReducer from "./reducers/noteReducer";
+import noteReducer, { createNote } from "./reducers/noteReducer";
+import filterReducer, { filterChange } from "./reducers/filterReducer";
 
-const store = createStore(noteReducer);
+const reducer = combineReducers({
+  notes: noteReducer,
+  filter: filterReducer,
+});
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
-    <App />
-  </Provider>
-);
+const store = createStore(reducer);
+
+store.subscribe(() => console.log(store.getState()))
+store.dispatch(filterChange('IMPORTANT'))
+store.dispatch(createNote('combineReducers forms one reducer from many simple reducers'))
+store.dispatch(filterChange('NONIMPORTANT'))
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <Provider store={store}>
+//     {/* <App /> */}
+//     <div />
+//   </Provider>
+// );
