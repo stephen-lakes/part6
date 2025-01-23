24abefad82b11de0ac62028aac2a83d1ee250018
stephen-lakes <oluyomiolamilekan99@gmail.com>
2025-01-23 03:03:45 +0100

    use VisibilityFilter in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,13 @@
 import "./App.css";
 import NewNote from "./components/NewNote";
 import Notes from "./components/Notes";
+import VisibilityFilter from "./components/VisibilityFilter"
 
 const App = () => {
-  const filterSelected = (value) => {
-    console.log(value);
-  };
-
   return (
     <div>
       <NewNote />
-      <div>
-        all
-        <input
-          type="radio"
-          name="filter"
-          id=""
-          onChange={() => filterSelected("ALL")}
-        />
-        important
-        <input
-          type="radio"
-          name="filter"
-          id=""
-          onChange={() => filterSelected("NIMPORTANT")}
-        />
-        nonimportant
-        <input
-          type="radio"
-          name="filter"
-          id=""
-          onChange={() => filterSelected("NONIMPORTANT")}
-        />
-      </div>
+      <VisibilityFilter />
       <Notes />
     </div>
   );
