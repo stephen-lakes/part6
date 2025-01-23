6a1a349c63ab2c3b62e536d76ea3342c57b35d66
stephen-lakes <oluyomiolamilekan99@gmail.com>
2025-01-23 03:17:53 +0100

    refactor: Improve note filtering logic with destructured state
    - Updated useSelector in Notes component to use state destructuring for
      notes and filter
      - Enhanced readability and maintainability of state selection logic
      - Improved conditional filtering of notes based on visibility filter
        state
      - Ensured proper display of all, important, and nonimportant notes

--- src/components/Notes.jsx
+++ src/components/Notes.jsx
@@ -22,13 +22,13 @@ Note.propTypes = {
 
 const Notes = () => {
   const dispatch = useDispatch();
-  const notes = useSelector((state) => {
-    if (state.filter === "ALL") {
-      return state.notes;
+  const notes = useSelector(({ notes, filter }) => {
+    if (filter === "ALL") {
+      return notes;
     }
-    return state.filter === "IMPORTANT"
-      ? state.notes.filter((note) => note.important)
-      : state.notes.filter((note) => !note.important);
+    return filter === "IMPORTANT"
+      ? notes.filter((note) => note.important)
+      : notes.filter((note) => !note.important);
   });
 
   return (
