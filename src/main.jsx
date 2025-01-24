d4fd6cce1bd23998a3df2167e700adb27cff1bcb
stephen-lakes <oluyomiolamilekan99@gmail.com>
2025-01-25 00:31:35 +0100

    use @Freduxjs/toolkit
    use configureStore from @reduxjs/toolkit

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,19 @@
 import ReactDOM from "react-dom/client";
 
-import { createStore, combineReducers } from "redux";
 import App from "./App";
 import { Provider } from "react-redux";
 import noteReducer from "./reducers/noteReducer";
 import filterReducer from "./reducers/filterReducer";
+import { configureStore } from "@reduxjs/toolkit";
 
-const reducer = combineReducers({
-  notes: noteReducer,
-  filter: filterReducer,
+const store = configureStore({
+  reducer: {
+    notes: noteReducer,
+    filter: filterReducer,
+  },
 });
 
-const store = createStore(reducer);
+console.log(store.getState());
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
