2c49897c654a4d25c466245f58afc13491859add
stephen-lakes <oluyomiolamilekan99@gmail.com>
2025-01-25 00:29:33 +0100

    Add notes slice with create and toggle importance reducers
    - Added a notes slice with initial state and reducers
    - Implemented the createNote reducer to add new notes
    - Implemented the toggleImportanceOf reducer to toggle the importance of
      notes

--- src/reducers/noteReducer.js
+++ src/reducers/noteReducer.js
@@ -1,3 +1,5 @@
+import { createSlice } from "@reduxjs/toolkit";
+
 const initialState = [
   {
     content: "reducer defines how redux store works",
@@ -11,42 +13,31 @@ const initialState = [
   },
 ];
 
-const noteReducer = (state = initialState, action) => {
-  if (action.type === "NEW_NOTE") {
-    return state.concat(action.payload);
-  }
+const generateId = () => Number((Math.random() * 1000000).toFixed(0));
 
-  if (action.type === "TOGGLE_IMPORTANCE") {
-    const id = action.payload.id;
-    const noteToChange = state.find((n) => n.id === id);
-    const changedNote = {
-      ...noteToChange,
-      important: !noteToChange.important,
-    };
-    return state.map((note) => (note.id !== id ? note : changedNote));
-  }
-
-  return state;
-};
-
-export const generateId = () => Number((Math.random() * 1000000).toFixed(0));
-
-export const createNote = (content) => {
-  return {
-    type: "NEW_NOTE",
-    payload: {
-      content,
-      important: false,
-      id: generateId(),
+const noteSlice = createSlice({
+  name: "notes",
+  initialState: initialState,
+  reducers: {
+    createNote(state, action) {
+      const content = action.payload;
+      state.push({
+        content,
+        important: false,
+        id: generateId(),
+      });
     },
-  };
-};
+    toggleImportanceOf(state, action) {
+      const id = action.payload;
+      const noteToChange = state.find((note) => note.id === id);
+      const changedNote = {
+        ...noteToChange,
+        important: !noteToChange.important,
+      };
+      state.map((note) => (note.id !== id ? note : changedNote));
+    },
+  },
+});
 
-export const toggleImportanceOf = (id) => {
-  return {
-    type: "TOGGLE_IMPORTANCE",
-    payload: { id },
-  };
-};
-
-export default noteReducer;
+export const { createNote, toggleImportanceOf } = noteSlice.actions;
+export default noteSlice.reducer;
