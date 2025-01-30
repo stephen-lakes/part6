12d0301eaaf6cbd775cb4227080328605ffe8f71
stephen-lakes <oluyomiolamilekan99@gmail.com>
2025-01-30 17:19:06 +0100

    Implemented appendNote function to add new notes to the state
    - Added appendNote function to handle new notes
    - The function takes state and action as parameters
    - Pushes the new note (action.payload) to the state array

--- src/reducers/noteReducer.js
+++ src/reducers/noteReducer.js
@@ -1,23 +1,23 @@
 import { createSlice, current } from "@reduxjs/toolkit";
 
-const initialState = [
-  {
-    content: "reducer defines how redux store works",
-    important: true,
-    id: 1,
-  },
-  {
-    content: "state of store can contain any data",
-    important: false,
-    id: 2,
-  },
-];
+// const initialState = [
+//   {
+//     content: "reducer defines how redux store works",
+//     important: true,
+//     id: 1,
+//   },
+//   {
+//     content: "state of store can contain any data",
+//     important: false,
+//     id: 2,
+//   },
+// ];
 
 const generateId = () => Number((Math.random() * 1000000).toFixed(0));
 
 const noteSlice = createSlice({
   name: "notes",
-  initialState: initialState,
+  initialState: [],
   reducers: {
     createNote(state, action) {
       const content = action.payload;
@@ -34,11 +34,14 @@ const noteSlice = createSlice({
         ...noteToChange,
         important: !noteToChange.important,
       };
-      console.log(current(state))
+      console.log(current(state));
       return state.map((note) => (note.id !== id ? note : changedNote));
     },
   },
+  appendNote(state, action) {
+    state.push(action.payload);
+  },
 });
 
-export const { createNote, toggleImportanceOf } = noteSlice.actions;
+export const { createNote, toggleImportanceOf, appendNote } = noteSlice.actions;
 export default noteSlice.reducer;
