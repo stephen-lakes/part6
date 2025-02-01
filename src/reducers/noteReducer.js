2c595974295fa3ebc48d7aaa76a12990b907e26a
stephen-lakes <oluyomiolamilekan99@gmail.com>
2025-02-01 22:46:48 +0100

    refactor: Simplify toggleImportanceOf reducer logic
    - Simplified the `toggleImportanceOf` reducer by removing unnecessary
      lines
    - Directly maps state to update note's importance
    - Added logging to check the current state

--- src/reducers/noteReducer.js
+++ src/reducers/noteReducer.js
@@ -29,14 +29,15 @@ const noteSlice = createSlice({
       state.push(action.payload);
     },
     toggleImportanceOf(state, action) {
-      const id = action.payload;
-      const noteToChange = state.find((note) => note.id === id);
-      const changedNote = {
-        ...noteToChange,
-        important: !noteToChange.important,
-      };
+      const toggledNote = action.payload;
+      // const noteToChange = state.find((note) => note.id === toggledNote.id);
+      // const changedNote = {
+      //   ...noteToChange,
+      //   important: !noteToChange.important,
+      // };
       console.log(current(state));
-      return state.map((note) => (note.id !== id ? note : changedNote));
+      return state.map((note) => (note.id !== toggledNote.id ? note : toggledNote));
+
     },
     appendNote(state, action) {
       state.push(action.payload);
