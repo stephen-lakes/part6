917a9a5904a8490a36d23428572119f90d95643f
stephen-lakes <oluyomiolamilekan99@gmail.com>
2025-02-03 14:42:45 +0100

    chore: Move code related to store creation to store.js

--- src/main.jsx
+++ src/main.jsx
@@ -2,26 +2,7 @@ import ReactDOM from "react-dom/client";
 
 import App from "./App";
 import { Provider } from "react-redux";
-import noteReducer, { setNotes } from "./reducers/noteReducer";
-import filterReducer from "./reducers/filterReducer";
-import { configureStore } from "@reduxjs/toolkit";
-
-import noteService from "./services/notes";
-
-const store = configureStore({
-  reducer: {
-    notes: noteReducer,
-    filter: filterReducer,
-  },
-});
-
-// noteService.getAll().then((notes) => {
-//   notes.forEach((note) => {
-//     store.dispatch(appendNote(note));
-//   });
-// });
-
-// noteService.getAll().then((notes) => store.dispatch(setNotes(notes)));
+import store from "./store";
 
 console.log(store.getState());
 
