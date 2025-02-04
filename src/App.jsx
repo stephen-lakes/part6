5c73154cf5f5b4a112b26815f15fde667851243c
stephen-lakes <oluyomiolamilekan99@gmail.com>
2025-02-04 09:03:08 +0100

    feat: Implement data fetching with react-query
    - Integrated @tanstack/react-query for managing server state
    - Added useQuery hook to fetch notes from the server
    - Updated App component to display notes from the server

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,45 @@
-import { useDispatch } from "react-redux";
-import { useEffect } from "react";
-import "./App.css";
-import NewNote from "./components/NewNote";
-import Notes from "./components/Notes";
-import VisibilityFilter from "./components/VisibilityFilter";
-import noteService from "./services/notes";
-import { initializeNotes, setNotes } from "./reducers/noteReducer";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 
 const App = () => {
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(initializeNotes());
-  }, []);
+  const addNote = async (event) => {
+    event.preventDefault();
+    const content = event.target.note.value;
+    event.target.note.value = "";
+    console.log(content);
+  };
+
+  const toggleImportance = (note) => {
+    console.log("toggle importance of", note.id);
+  };
+
+  const result = useQuery({
+    queryKey: ["notes"],
+    queryFn: () =>
+      axios.get("http://localhost:3001/notes").then((res) => res.data),
+  });
+
+  console.log("2", JSON.parse(JSON.stringify(result)));
+
+  if (result.isLoading) {
+    return <div>Loading ...</div>;
+  }
+
+  const notes = result.data;
 
   return (
     <div>
-      <NewNote />
-      <VisibilityFilter />
-      <Notes />
+      <h2>Notes app</h2>
+      <form onSubmit={addNote}>
+        <input name="note" />
+        <button type="submit">add</button>
+      </form>
+      {notes.map((note) => (
+        <li key={note.id} onClick={() => toggleImportance(note)}>
+          {note.content}
+          <strong> {note.important ? "important" : ""}</strong>
+        </li>
+      ))}
     </div>
   );
 };
