839eb948103ff841a94e6248b88265b7a3093e8d
stephen-lakes <oluyomiolamilekan99@gmail.com>
2025-02-04 09:01:21 +0100

    feat: Integrate react-query for data fetching
    - Added QueryClient and QueryClientProvider from @tanstack/react-query
    - Wrapped App component with QueryClientProvider for state management

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,12 @@
 import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import App from "./App";
-import { Provider } from "react-redux";
-import store from "./store";
 
-console.log(store.getState());
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
+  <QueryClientProvider client={queryClient}>
     <App />
-  </Provider>
+  </QueryClientProvider>
 );
